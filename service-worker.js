e550ab85f384468c0bf706e5a8a0673709fb9672
FremderL <161394742+FremderL@users.noreply.github.com>
2024-10-15 16:59:40 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const urlsToCache = [
   '/index.html',
   '/estilo/style.css',
   '/script/main.js',
-  '/imagen/icon-192x192.png', // Asegúrate de que estas rutas sean correctas
-  '/imagen/icon-512x512.png',
+  '/imagen/icon192.png', // Asegúrate de que estas rutas sean correctas
+  '/imagen/icon512.png',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
 ];
